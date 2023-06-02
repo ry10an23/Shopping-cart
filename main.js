cf162afe87ca95341c349da538d2474b8c99722e
ry10an23 <kawaguchi.r823@gmail.com>
2023-06-01 22:16:42 -0700

    define the variables and classes

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
 "use strict";
+
+// Variables
+const cartBtn = document.querySelector(".cart-btn");
+const closeCartBtn = document.querySelector(".close-cart");
+const clearCartBtn = document.querySelector(".clear-cart");
+const cartDOM = document.querySelector(".cart");
+const cartOverlay = document.querySelector(".cart-overlay");
+const cartItems = document.querySelector(".cart-items");
+const cartTotal = document.querySelector(".cart-total");
+const cartContent = document.querySelector(".cart-content");
+const productsDom = document.querySelector(".products-centre");
+
+// Cart
+let cart = [];
+
+//  Getting the Products
+class Products {}
+
+// Display Products
+class UI {}
+
+// Local Storage
+class Storage {}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const ui = new UI();
+  const products = new Products();
+}); //DOMContentLoaded : Event fired when the HTML page has finished loading and the DOM tree construction is complete.
