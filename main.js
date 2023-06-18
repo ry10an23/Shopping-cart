50d473ba1e155068decb8eb72aec03e73214c81f
ry10an23 <kawaguchi.r823@gmail.com>
2023-06-17 21:19:21 -0700

    change the price and number of items in the cart

--- main.js
+++ main.js
@@ -156,6 +156,14 @@ class UI {
         let id = removeItem.dataset.id;
         cartContent.removeChild(removeItem.parentElement.parentElement);
         this.removeItem(id);
+      } else if (e.target.classList.contains("fa-chevron-up")) {
+        let addAmount = e.target;
+        let id = addAmount.dataset.id;
+        let tempItem = cart.find((item) => item.id == id);
+        tempItem.amount = tempItem.amount + 1;
+        Storage.saveCart(cart);
+        this.setCartValues(cart);
+        addAmount.nextElementSibling.innerText = tempItem.amount;
       }
     });
   }
